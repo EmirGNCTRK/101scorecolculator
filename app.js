23a890cc9e1497c99673a8dcee9d617df91bb1f0
EmirGNCTRK <97238958+EmirGNCTRK@users.noreply.github.com>
2022-03-22 22:08:21 +0300

    123

---
+++ app.js
@@ -0,0 +1,56 @@
+var takım1 = 0 ;
+var takım2 = 0 ;
+var el = 1 ;
+var say = 1 ;
+
+let formDOM = document.querySelector("#Form")
+formDOM.addEventListener("submit",puan)
+
+// function puan(event){
+//     event.preventDefault()
+//     let girdi = document.querySelector("#input1").value
+//     // console.log(girdi);
+//     takım1 += parseInt(girdi) ;
+//     // console.log(takım1);
+//     localStorage.getItem(input1,girdi.value)
+//     document.querySelector(".con3").innerHTML = takım1;
+    
+//     document.querySelector(".con1").innerHTML +=`${el}.el=(${girdi})|!|   `
+//     // document.getElementById("Form").style.display = "none";
+//     // document.getElementById("Form2").style.display = "";
+//     let button = document.querySelector(".abcd");
+//     button.click()   
+// }
+
+
+function puan(event){
+    event.preventDefault()
+    let girdi = document.querySelector("#input").value;
+    localStorage.getItem(input,girdi.value)
+    if(girdi === ""){
+        alert("PLEASE ENTER NUMBER!!")
+    }
+
+    else{
+
+        if(say > 0){
+            takım1 += parseInt(girdi) ;
+            document.querySelector(".con1").innerHTML +=`${el}.el=(${girdi})|!|   `
+            let button = document.querySelector(".abcd");button.click()
+            document.querySelector("#takımsay").innerHTML = "TEAM 2"
+            document.querySelector(".con3").innerHTML = takım1;
+            say += -2
+        }
+        else{
+            takım2 += parseInt(girdi) ;
+            document.querySelector(".con2").innerHTML +=`${el}.el=(${girdi})|!|   `
+            let button = document.querySelector(".abcd");button.click()
+            document.querySelector(".con4").innerHTML = takım2;
+            document.querySelector("#takımsay").innerHTML = "TEAM 1"
+            say += 2
+            el += 1
+            document.querySelector("#el").innerHTML = `${el-1}.el bitti`
+        }
+        
+    }
+}
